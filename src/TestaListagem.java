38a6b1ff89de0a6f5c3be26512336e194182f49c
JulianaOnofrio <julianaonofrio@hotmail.com>
2020-12-22 10:09:53 -0300

    Implementação de PreparedStatement

---
+++ src/TestaListagem.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+
+public class TestaListagem {
+
+    public static void main(String[] args) throws SQLException {
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        Connection connection = connectionFactory.recuperarConexao();
+
+        PreparedStatement stm = connection.prepareStatement("SELECT ID, NOME, DESCRICACAO FROM PRODUTO ");
+        stm.execute();
+
+
+        ResultSet rst = stm.getResultSet();
+
+        while (rst.next()) {
+            Integer id = rst.getInt("ID");
+            System.out.println(id);
+            String nome = rst.getString("NOME");
+            System.out.println(nome);
+            String descricacao = rst.getString("DESCRICACAO");
+            System.out.println(descricacao);
+
+        }
+        connection.close();
+
+    }
+
+}
+
