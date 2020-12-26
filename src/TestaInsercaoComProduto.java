62fe950be87009767112ba5e9c926d48a9703dfb
JulianaOnofrio <julianaonofrio@hotmail.com>
2020-12-26 02:24:29 -0300

    DAO com inserção do produto

--- src/TestaInsercaoComProduto.java
+++ src/TestaInsercaoComProduto.java
@@ -1,3 +1,4 @@
+import br.com.alura.jdbc.dao.ProdutoDAO;
 import br.com.alura.jdbc.factory.ConnectionFactory;
 import br.com.alura.jdbc.modelo.Produto;
 
@@ -9,24 +10,8 @@ public class TestaInsercaoComProduto {
         Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
 
         try (Connection connection = new ConnectionFactory().recuperarConexao()) {
-
-            String sql = "INSERT INTO PRODUTO (NOME,DESCRICACAO) VALUES (?, ?)";
-
-            try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-
-                pstm.setString(1, comoda.getNome());
-                pstm.setString(2, comoda.getDescricacao());
-
-                pstm.execute();
-
-                try(ResultSet rst = pstm.getGeneratedKeys()) {
-                    while(rst.next()) {
-                        comoda.setId(rst.getInt(1));
-                    }
-                }
-            }
+            ProdutoDAO produtoDao = new ProdutoDAO(connection);
+           produtoDao.salvar(comoda);
         }
-
-        System.out.println(comoda);
     }
 }
